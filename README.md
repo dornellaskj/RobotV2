207254da2b003c685d4b989d34d54efa3e3d829b
Kevin D'Ornellas <dornellaskj@gmail.com>
2022-12-25 12:54:17 -0700

    trying pygame for sound

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Python application to run the robot created by me and my daughter.
 1. https://github.com/SequentMicrosystems/megaio-rpi/blob/master/python/megaio/README.md
 clone and install, then you will need to turn on I2E in rpi config.
 
-2. pip install playsound
+2. pip install pygame
 
--- talk.py
+++ talk.py
@@ -1,5 +1,12 @@
+import time
+from pygame import mixer
+
 from playsound import playsound
 
 def whoami():
- playsound('./gun_robot.mp3')
+    mixer.init()
+    mixer.music.load("./gun_robot.mp3")
+    mixer.music.play()
+    while mixer.music.get_busy():  # wait for music to finish playing
+        time.sleep(1)
 
