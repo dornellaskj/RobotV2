25220ac6653ff64cddad39dbc605e459421a46ad
Kevin D'Ornellas <dornellaskj@gmail.com>
2022-12-27 08:36:20 -0700

    minor test fix

--- test.py
+++ test.py
@@ -13,14 +13,14 @@ def test(pin):
     megaio.set_relays(0,0)
 
 def down(pin):
-
+    megaio.set_relays(0,pin)
     power.backward()
     time.sleep(.2)
     power.off()
     megaio.set_relays(0,0)
 
 def up(pin):
-
+    megaio.set_relays(0,pin)
     power.forward()
     time.sleep(.2)
     power.off()
