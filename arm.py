6bdcbd395b4b46697cd7d63c3fb07b7790b8280f
Kevin D'Ornellas <dornellaskj@gmail.com>
2022-12-27 08:14:50 -0700

    adding arm movements

--- arm.py
+++ arm.py
@@ -1,2 +1,16 @@
-32 arm elbow
-1 arm shoulder
+import power
+import megaio
+
+
+ELBOW = 32
+SHOULDER = 1
+
+def move(direction, part):
+    if direction = 1:
+        power.forward()
+    if direction = 0:
+        power.backward()
+    megaio.set_relays(0, part)
+
+def shoulder_up():
+    move(1, SHOULDER)
