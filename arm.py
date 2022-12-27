a93d2297a3b5fc3409d2d4eeb4b21343c7d09f20
Kevin D'Ornellas <dornellaskj@gmail.com>
2022-12-27 08:26:02 -0700

    removing unnecessary classes

--- arm.py
+++ arm.py
@@ -4,20 +4,15 @@ import time
 
 ELBOW = 32
 SHOULDER = 1
-class Base:
-    def __move(direction, part, time):
-        if direction == 1:
-            power.forward()
-        if direction == 0:
-            power.backward()
-        megaio.set_relays(0, part)
-        time.sleep(time)
-        power.off()
+def __move(direction, part, time):
+    if direction == 1:
+        power.forward()
+    if direction == 0:
+        power.backward()
+    megaio.set_relays(0, part)
+    time.sleep(time)
+    power.off()
 
-class Arm(Base):
-    def __init__(self):
-        Base.__init__(self)
-        
-    def shoulder_up(time):
-        self.__move(1, SHOULDER, time)
+def shoulder_up(time):
+    self.__move(1, SHOULDER, time)
     
