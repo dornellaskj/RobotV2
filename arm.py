7d9e0bf773465cc88d0f79394f9a161868e8cc01
Kevin D'Ornellas <dornellaskj@gmail.com>
2022-12-27 08:21:59 -0700

    making move private method

--- arm.py
+++ arm.py
@@ -1,16 +1,23 @@
 import power
 import megaio
-
+import time
 
 ELBOW = 32
 SHOULDER = 1
+class Base:
+    def __move(direction, part, time):
+        if direction == 1:
+            power.forward()
+        if direction == 0:
+            power.backward()
+        megaio.set_relays(0, part)
+        time.sleep(time)
+        power.off()
 
-def move(direction, part):
-    if direction == 1:
-        power.forward()
-    if direction == 0:
-        power.backward()
-    megaio.set_relays(0, part)
-
-def shoulder_up():
-    move(1, SHOULDER)
+class Arm(Base):
+    def __init__(self):
+        Base.__init__(self)
+        
+    def shoulder_up(time):
+        self.__move(1, SHOULDER, time)
+    
